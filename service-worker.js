7593d345e2f24e7135a940bfa4af7636af369b32
Aditya007-ind <adityajagtap0308@gmail.com>
2025-07-24 22:58:50 +0530

    Updates

---
+++ service-worker.js
@@ -0,0 +1,14 @@
+// public/service-worker.js
+
+self.addEventListener('install', event => {
+  console.log('✅ Service Worker Installed');
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  console.log('✅ Service Worker Activated');
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(fetch(event.request));
+});
